68a3ed0202fe4de083d4ca736c98002a064b036c
Valeria Espinoza <76666534+ValDLaw@users.noreply.github.com>
2023-07-09 15:27:09 -0500

    KNN-Secuencial PQ test

--- backend/knn_secuencial.py
+++ backend/knn_secuencial.py
@@ -1,13 +1,20 @@
 import face_recognition
+import heapq
 
 def knn_pq(faces_encoding, dataset, k):
     result = []
+    heapq.heapify(result)
     for path, matrix_vector_faces in dataset:
         for distance in face_recognition.face_distance(matrix_vector_faces, faces_encoding[0]):
             result.append((path, distance))
 
-    result = sorted(result, key = lambda x: x[1])
-    return [path for path, distance in result[:k]]
+    resultK=[]
+    for n in range(k):
+        if n <= k:
+            resultK.append(heapq.heappop(result))
+        else:
+            break
+    return [path for path, distance in resultK]
 
 
 def range_search(faces_encoding, dataset, radio):
