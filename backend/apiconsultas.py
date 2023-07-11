b051bb3481c9f18d11071994debc8d8f2b6cc9cd
Valentín Quezada <hector.quezada@utec.edu.pe>
2023-07-10 19:37:29 -0500

    api funcional

--- backend/apiconsultas.py
+++ backend/apiconsultas.py
@@ -13,6 +13,8 @@ from PIL import Image
 
 from knn_secuencial import knn_pq
 from knn_rtree import knn_rtree
+from knn_kdtree import KDTree_index, KDTree_search
+from knn_faiss import FaissIndex_Search
 
 os.environ['UPLOAD_FOLDER'] = 'uploads/'
 
@@ -20,6 +22,8 @@ app = Flask(__name__)
 CORS(app, origin="*")
 dataset = extraccion_de_caracteristicas.load_json()
 app.config["UPLOAD_FOLDER"] = os.environ.get("UPLOAD_FOLDER")
+
+N = 12800
 
 
 @app.route("/sequential", methods=["POST"])
@@ -34,7 +38,7 @@ def upload_image_sequential():
     face_encodings = face_recognition.face_encodings(query_image)
     k = int(request.form['k'])
 
-    return knn_pq(face_encodings, dataset, k)
+    return knn_pq(face_encodings, dataset[:N], k)
 
 @app.route("/rtree", methods=["POST"])
 def upload_image_rtree():
@@ -48,12 +52,41 @@ def upload_image_rtree():
     face_encodings = face_recognition.face_encodings(query_image)
     k = int(request.form['k'])
 
-    return knn_rtree(face_encodings, dataset, k, 12800)
+    return knn_rtree(face_encodings, dataset, k, N)
+
+@app.route("/kdtree", methods=["POST"])
+def upload_image_kdtree():
+    file = request.files["file"]
+    random_seed = ShortUUID().random(length=50)
+    img_id = secure_filename(str(random_seed) + str(file.filename))
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], img_id))
+
+    #image_path = request.form['image_path']
+    query_image = face_recognition.load_image_file(file)
+    face_encodings = face_recognition.face_encodings(query_image)
+    k = int(request.form['k'])
+
+    index = KDTree_index(dataset[:N])
+    return KDTree_search(face_encodings, dataset, k, index)
+
+@app.route("/faiss", methods=["POST"])
+def upload_image_faiss():
+    file = request.files["file"]
+    random_seed = ShortUUID().random(length=50)
+    img_id = secure_filename(str(random_seed) + str(file.filename))
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], img_id))
+
+    #image_path = request.form['image_path']
+    query_image = face_recognition.load_image_file(file)
+    face_encodings = face_recognition.face_encodings(query_image)
+    k = int(request.form['k'])
+
+    return FaissIndex_Search(face_encodings, dataset, k, N)
 
 @app.route("/")
 def index():
     print("Ruta raíz accedida")
-    return "Api para retrieve propio"
+    return "Api para subir imágenes"
 
 if __name__ == '__main__':
     app.run()
