7e63905b52b7a5d53521bb7d29d00126faf7f4c0
Valentín Quezada <hector.quezada@utec.edu.pe>
2023-07-10 20:38:54 -0500

    sequential diego

--- backend/knn_secuencial.py
+++ backend/knn_secuencial.py
@@ -1,9 +1,12 @@
 import face_recognition
-import heapq
+import heapq as pq
+from extraccion_de_caracteristicas import formateoPath
 
+'''
+VALERIA:
 def knn_pq(faces_encoding, dataset, k):
     result = []
-    heapq.heapify(result)
+    pq.heapify(result)
     for path, matrix_vector_faces in dataset:
         for distance in face_recognition.face_distance(matrix_vector_faces, faces_encoding):
             result.append((path, distance))
@@ -14,7 +17,7 @@ def knn_pq(faces_encoding, dataset, k):
             resultK.append(heapq.heappop(result))
         else:
             break
-    res = heapq.nsmallest(k , result)
+    res = pq.nsmallest(k , result)
     print(result)
     print(res)
     return [path for path, distance in resultK]
@@ -29,3 +32,44 @@ def range_search(faces_encoding, dataset, radio):
 
     result = sorted(result, key = lambda x: x[1])
     return [path for path, dis in result]
+'''
+
+ERES_VALERIA = False
+
+def knn_pq(faces_encoding, dataset, k):
+    result = []
+    for path, matrix_vector_faces in dataset:
+        for distance in face_recognition.face_distance(matrix_vector_faces, faces_encoding):
+            #result.append((os.path.basename(path), distance))
+            pq.heappush(result, (distance, formateoPath(path, ERES_VALERIA)))
+
+    resultK = pq.nsmallest(k , result)
+
+    return [formateoPath(path, ERES_VALERIA) for distance, path in resultK]
+
+def select_representative_radio(mean_distance, std_distance):
+    # regla empírica
+    r1 = mean_distance - std_distance
+    r2 = mean_distance - 2 * std_distance
+    r3 = mean_distance - 3 * std_distance
+
+    return [r1, r2, r3]
+
+def distances(dataset):
+    vector_dist = []
+    for path, matrix_vector_faces in dataset:
+        for distance in face_recognition.face_distance(matrix_vector_faces, faces_encoding):
+            vector_dist.append(distance)
+
+    return list((np.mean(vector_dist), np.std(vector_dist)))
+
+def range_search(faces_encoding, dataset, radio):
+    result = []
+    for path, matrix_vector_faces in dataset:
+        for distance in face_recognition.face_distance(matrix_vector_faces, faces_encoding):
+            if distance < radio:
+                result.append(( formateoPath(path, ERES_VALERIA), distance))
+
+    if len(result):
+        result = sorted(result, key = lambda x: x[1])
+        return [path for path, dis in result]
